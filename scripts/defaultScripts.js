3276363a41b48d54a5852f8884b00159a0f1d5a4
Chris Collins <chris@highfidelity.io>
2017-09-28 12:41:15 -0700

    Only remove menu
    Only remove menu

--- scripts/defaultScripts.js
+++ scripts/defaultScripts.js
@@ -16,10 +16,17 @@ var DEFAULT_SCRIPTS_COMBINED = [
     "system/away.js",
     "system/audio.js",
     "system/hmd.js",
+    "system/bubble.js",
+    "system/snapshot.js",
+    "system/help.js",
+    "system/pal.js", // "system/mod.js", // older UX, if you prefer
+    "system/makeUserConnection.js",
     "system/tablet-goto.js",
     "system/marketplaces/marketplaces.js",
+    "system/commerce/wallet.js",
     "system/edit.js",
     "system/notifications.js",
+    "system/dialTone.js",
     "system/firstPersonHMD.js",
     "system/tablet-ui/tabletUI.js"
 ];
